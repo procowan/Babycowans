151b163817442076c78b0f144f595e3fca5a4a70
Saman Sabbaghi <saman.sabbaghi1992@gmail.com>
2026-08-03 08:05:20 -0400

    feat(protocol): bootstrap Anchor protocol

---
+++ protocol/babycowans-protocol/programs/babycowans-protocol/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("BSZkHJyqBW19HQ2tTgooKxPc5FEehgm5uxL44Ggxjucp");
+
+#[program]
+pub mod babycowans_protocol {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
